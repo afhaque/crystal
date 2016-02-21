15fe7600c5cdac0618c898b0703e3af4a1fd8118
Neena Rinaldi <Neena@192.168.1.12>
2016-02-21 17:44:06 -0500

    broken code with unclickable images

---
+++ game.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$("#blueGem").on('click' , function(){
+		alert('hi');
+	});
+
+	$("#greenGem").on('click' , function(){
+		alert('hi');
+	});
+
+	$("#redGem").on('click' , function(){
+		alert('hi');
+	});
+
+	$("#yellowGem").on('click' , function(){
+		alert('hi');
+	});
+})
